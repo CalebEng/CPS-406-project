55d552a3d50ca2f1a32c9c4797d478d51b459389
Ayman <89948899+amohamma8029@users.noreply.github.com>
2023-03-23 11:30:42 -0400

    added collections for all data, and modified class file

---
+++ Database.java
@@ -0,0 +1,16 @@
+/* 
+Contributors: Ayman Mohammad
+Description: This class contains and handles the data that will be stored and used within the shopping system. 
+*/
+
+import java.util.ArrayList;
+
+public class Database {
+    private ArrayList<User> usersCollection;
+    private ArrayList<Shoe> productsCollection;
+    private ArrayList<Order> ordersCollection;
+    private ArrayList<Review> reportsCollection;
+
+    public Database(){
+    }
+}
