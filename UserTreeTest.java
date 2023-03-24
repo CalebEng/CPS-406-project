e3aa8c32f726afc005e4d7516602f7cf74123b81
CalebEng <121829627+CalebEng@users.noreply.github.com>
2023-03-24 13:44:55 -0400

    Added and changed tests slightly

--- UserTreeTest.java
+++ UserTreeTest.java
@@ -7,21 +7,24 @@
 import java.util.ArrayList;
 public class UserTreeTest {
     public static void main (String[]args){
-        Seller sellTest = new Seller("GenName", "Woit@tmu.ca", "Woit is the best", "cps590 is the best course ever");
-        Admin  adTest = new Admin("idk something", "imAnAdmin@tmu.ca", "password!");
-        Shopper shopTest = new Shopper("Woits biggest fan", "1234 street", "passwordShopper", "gmai.com2");
-        Shopper shopTest2 = new Shopper("joe", "some street", "911", "passwordshopper2", "gmai.com");
-    
-        System.out.println("Seller test:");
-        System.out.println(sellTest);
-        
-        System.out.println("Admin test:");
-        System.out.println(adTest);
+      //  Seller sellTest = new Seller("GenName", "Woit@tmu.ca", "Woit is the best", "cps590 is the best course ever");
+        //Admin  adTest = new Admin("idk something", "imAnAdmin@tmu.ca", "password!");
+        //Shopper shopTest = new Shopper("Woits biggest fan", "1234 street", "passwordShopper", "gmai.com2");
+        //Shopper shopTest2 = new Shopper("joe", "some street", "911", "passwordshopper2", "gmai.com");
+        User userTest = new User("woit", "We love Woit", "Cps590 is great", "user");
 
-        System.out.println("Shopper 1 test:");
-        System.out.println(shopTest);
 
-        System.out.println("Shopper 2 test:");
-        System.out.println(shopTest2);
+        System.out.println("User test:\n"+userTest);
+  //      System.out.println("Seller test:");
+     //   System.out.println(sellTest);
+      
+    //    System.out.println("Admin test:");
+      //  System.out.println(adTest);
+
+        //System.out.println("Shopper 1 test:");
+        //System.out.println(shopTest);
+
+        //System.out.println("Shopper 2 test:");
+        //System.out.println(shopTest2);
     }
 }
